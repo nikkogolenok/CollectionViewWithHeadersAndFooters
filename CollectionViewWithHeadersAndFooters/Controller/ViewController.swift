4788b914f09ce7c2aa14d63542992a5b62913b12
nikita_kogolenok <nikiton17ns@gmail.com>
2022-03-23 17:18:19 +0300

    Created collectionView and extension with standart collection fethods

--- CollectionViewWithHeadersAndFooters/Controller/ViewController.swift
+++ CollectionViewWithHeadersAndFooters/Controller/ViewController.swift
@@ -9,9 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // MARK: - Variable
+    
+    // MARK: - GUI Variable
+    var collectionView: UICollectionView?
+    
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        createDelegateAndOther()
+    }
+    
+    // MARK: - Methods
+    func createDelegateAndOther() {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        layout.itemSize = CGSize(width: view.frame.size.width/2.2,
+                                 height: view.frame.size.width/2.2)
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView?.delegate = self
+        collectionView?.dataSource = self
+        view.addSubview(collectionView!)
     }
 }
-
---
+++ CollectionViewWithHeadersAndFooters/Extension/ViewController+Ext.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController+Ext.swift
+//  CollectionViewWithHeadersAndFooters
+//
+//  Created by Никита Коголенок on 23.03.22.
+//
+
+import UIKit
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "", for: indexPath)
+        
+        return cell
+    }
+}

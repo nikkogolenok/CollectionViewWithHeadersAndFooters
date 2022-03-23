7bb1d82fd7bd40b0712017878b6f670bf1be9d69
nikita_kogolenok <nikiton17ns@gmail.com>
2022-03-23 17:18:53 +0300

    Add Cell

---
+++ CollectionViewWithHeadersAndFooters/Cell/MyCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MyCollectionViewCell.swift
+//  CollectionViewWithHeadersAndFooters
+//
+//  Created by Никита Коголенок on 23.03.22.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}

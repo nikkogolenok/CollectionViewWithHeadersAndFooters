8d1f757519c234cb2591e759fe948b3952451917
nikita_kogolenok <nikiton17ns@gmail.com>
2022-03-23 16:48:27 +0300

    Initial Commit

---
+++ CollectionViewWithHeadersAndFooters/Controller/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  CollectionViewWithHeadersAndFooters
+//
+//  Created by Никита Коголенок on 23.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
